bd59c9dd5969335d88fff1d2ffe29c98ad57226b
howellk3 <khowellmobile@gmail.com>
2023-09-13 10:10:53 -0400

    Added pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
