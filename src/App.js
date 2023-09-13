101bbdc910e2b5e50f24d15d2d526f97e108da25
howellk3 <khowellmobile@gmail.com>
2023-09-13 10:24:17 -0400

    Added Navbar and css module

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new-meetup" element={<NewMeetupPage />} />
