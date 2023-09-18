8a79c3e29cad602129c6c002db05b39333e6993a
howellk3 <khowellmobile@gmail.com>
2023-09-18 10:19:11 -0400

    Added backend database using firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,31 @@
+import { useNavigate } from "react-router-dom"; 
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-    return <section>
-      <h1>Add New Meetup</h1>
-      <NewMeetupForm />
-    </section>;
+  const navigate = useNavigate();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-54929-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      navigate('/');
+    });
   }
-  
-  export default NewMeetupPage;
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
+
+export default NewMeetupPage;
