1860557ba919b0169b785f5519368b57cee5f1cf
howellk3 <khowellmobile@gmail.com>
2023-10-04 10:16:37 -0400

    removed uneeded div and extra styling

--- src/components/layout/Background.js
+++ src/components/layout/Background.js
@@ -5,22 +5,12 @@ import imageData from "../imageData/imageData";
 
 function Background(props) {
   const [currentImage, setCurrentImage] = useState(0);
-  const [opacity, setOpacity] = useState(1);
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    setCurrentImage(0);
+    setCurrentImage(2);
   }, []);
 
-  const backgroundStyle1 = {
-    backgroundImage: "url(" + imageData[currentImage + 1] + ")",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-    height: "100vh",
-    transition: "3s linear",
-  };
-
-  const backgroundStyle2 = {
+  const backgroundStyle = {
     backgroundImage: "url(" + imageData[currentImage] + ")",
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
@@ -29,9 +19,7 @@ function Background(props) {
   };
 
   return (
-    <div style={backgroundStyle1}>
-      <div style={backgroundStyle2}>{props.children}</div>
-    </div>
+      <div style={backgroundStyle}>{props.children}</div>
   );
 }
 
